3da7c27b22cfceb89ae6afdaeb72e1a3e2be7214
qamarmukhtar <qumerm1@gmail.com>
2019-07-10 19:50:45 +0530

    about us updated

--- src/components/About/Teamphotosection.js
+++ src/components/About/Teamphotosection.js
@@ -5,15 +5,15 @@ export default function Teamphotosection() {
         <div>
             <div class="card-group">
   <div class="card">
-    <img src="https://images.pexels.com/photos/1181677/pexels-photo-1181677.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="card-img-top" alt="..."/>
+    <img src="https://images.pexels.com/photos/879109/pexels-photo-879109.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="card-img-top" alt="..."/>
     <div class="card-body">
-      <h5 class="card-title">qamar</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <h5 class="card-title">QAMAR MUKHTAR</h5>
+      <p class="card-text">I love to coding and making apps website learning about coding and new technology from the world .</p>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
   <div class="card">
-    <img src="https://images.pexels.com/photos/879109/pexels-photo-879109.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="card-img-top" alt="..."/>
+    <img src="https://images.pexels.com/photos/1181677/pexels-photo-1181677.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="card-img-top" alt="..."/>
     <div class="card-body">
       <h5 class="card-title">Nadeem</h5>
       <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
