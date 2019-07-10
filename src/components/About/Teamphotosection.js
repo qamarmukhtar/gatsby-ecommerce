d8817a1a6fee616e682689ac8927e69c7351b8af
qamarmukhtar <qumerm1@gmail.com>
2019-07-10 19:59:49 +0530

    aout update

--- src/components/About/Teamphotosection.js
+++ src/components/About/Teamphotosection.js
@@ -15,7 +15,7 @@ export default function Teamphotosection() {
   <div class="card">
     <img src="https://images.pexels.com/photos/1181677/pexels-photo-1181677.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="card-img-top" alt="..."/>
     <div class="card-body">
-      <h5 class="card-title">Nadeem</h5>
+      <h5 class="card-title">Nadeem jamadar</h5>
       <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
@@ -23,7 +23,7 @@ export default function Teamphotosection() {
   <div class="card">
     <img src="https://images.pexels.com/photos/574077/pexels-photo-574077.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="card-img-top" alt="..."/>
     <div class="card-body">
-      <h5 class="card-title">Zakir</h5>
+      <h5 class="card-title">Abdul Khadar</h5>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
